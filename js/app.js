8ed0279de567da645d0e47c9c918d5994d0b3069
rudi <rudi@localhost.localdomainboxee.local>
2014-10-31 22:42:25 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//charsleft 
+
+$('#description').bind('keyup', function(e) {
+	var left = 200 - $(this).val().length;
+
+	$('.charleft').text(left)
+});
+
+
+$(document).on('click', '.addtopic', function() {
+	var $inputs = $('#addtopic :input');
+
+    // not sure if you wanted this, but I thought I'd add it.
+    // get an associative array of just the values.
+    var values = {};
+    $inputs.each(function() {
+        values[this.name] = $(this).val();
+    });
+    console.log(values)
+	//$('#secondModal').foundation('reveal', 'open');
+})
+
+//validation
